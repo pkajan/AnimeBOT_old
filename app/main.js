3d3311f9a671fc43b78411d4e82be5c27c92fa22
pKajan <sknitro@gmail.com>
2018-11-03 20:37:52 +0100

    counting

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const util = require('util');
+var dateFormat = require('dateformat');
 const CronJob = require('cron').CronJob;
 
 // Load up the discord.js library
@@ -8,9 +9,14 @@ const client = new Discord.Client();
 // Here we load the config.json file that contains our token and our prefix values.
 var config = require('../config.json');
 
+// Here we load the anime.json file that contains release dates of our anime shows.
+var anime = require('../anime.json');
+var one_week = 7 * 24 * 60 * 60 * 1000;
+
 /**************************************************************************/
 /* FUNCTIONS */
-function Logging(message, type = null) {
+function Logging(message, /**/) {
+    var type = arguments[arguments.length - 1];// last argument is type
     // create log console/file
     switch (type) {
         case "ERROR":
@@ -36,6 +42,18 @@ function trans(translatit, /**/) {
         args.push(arguments[i]);
     }
     return util.format.apply(util, args);
+}
+
+function weeks_needed(calc_date, weeks = 0) {
+    var today = new Date();
+    var week_date = new Date(calc_date).getTime();
+    //var one_week = 7 * 24 * 60 * 60 * 1000;
+
+    if (week_date < today) {
+        var calc_date2 = week_date + one_week;
+        return weeks_needed(calc_date2, weeks += 1)
+    }
+    return weeks;
 }
 /**************************************************************************/
 /* STARTUP THINGS */
@@ -89,6 +107,37 @@ client.on("message", async message => {
             message.channel.send(trans("SPAM") + i);
         }
         Logging(trans("cmd_spam", args[0]));
+    }
+
+    if (command === "anime") {
+        removeCallMsg(message);
+        var obj = (anime);
+        var anime_in_array = [];
+
+        for (i in obj) {
+            var valueToPush = {};
+            valueToPush.name = i;
+            valueToPush.year = obj[i]["year"];
+            valueToPush.month = obj[i]["month"];
+            valueToPush.day = obj[i]["day"];
+            valueToPush.hour = obj[i]["hour"];
+            valueToPush.minute = obj[i]["minute"];
+            valueToPush.second = obj[i]["second"];
+            anime_in_array.push(valueToPush);
+        }
+
+        //console.log(anime_in_array);
+        console.log("-----------------------------------");
+        anime_in_array.forEach(function (item) {
+
+            var json_date = new Date(item.year, (item.month - 1), item.day, item.hour, item.minute, item.second, 0);
+            var weeks = weeks_needed(json_date);
+            var countDownDate = json_date.getTime() + (one_week * weeks);
+
+            countDownDate = dateFormat(new Date(countDownDate.valueOf()), "dddd, dS, HH:MM"); // Saturday, 9th, 16:46
+            message.channel.send(`**${item.name}**: ` + countDownDate);
+            Logging(`**${item.name}**: ` + countDownDate);
+        });
     }
 
 });
