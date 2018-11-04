0478635386305d7bc8161cffd0026139042bff53
pKajan <sknitro@gmail.com>
2018-11-04 14:05:01 +0100

    sorted countdown

--- app/main.js
+++ app/main.js
@@ -10,7 +10,7 @@ const client = new Discord.Client();
 var config = require('../config.json');
 
 // Here we load the anime.json file that contains release dates of our anime shows.
-var anime = require('../anime.json');
+var anime_file = '../anime.json';
 var one_week = 7 * 24 * 60 * 60 * 1000;
 
 /**************************************************************************/
@@ -47,13 +47,21 @@ function trans(translatit, /**/) {
 function weeks_needed(calc_date, weeks = 0) {
     var today = new Date();
     var week_date = new Date(calc_date).getTime();
-    //var one_week = 7 * 24 * 60 * 60 * 1000;
-
     if (week_date < today) {
         var calc_date2 = week_date + one_week;
         return weeks_needed(calc_date2, weeks += 1)
     }
     return weeks;
+}
+
+function dateDiffInDays(a, b) {
+    // a and b are javascript Date objects
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
 /**************************************************************************/
 /* STARTUP THINGS */
@@ -110,8 +118,13 @@ client.on("message", async message => {
     }
 
     if (command === "anime") {
+        var anime = require(anime_file);
         removeCallMsg(message);
-        var obj = (anime);
+        var obj = anime;
+        var one_day = "```fix\nOne Day:```\n";
+        var two_days = "\n```fix\nTwo Days:```\n";
+        var oth_days = "\n```fix\nLater:```\n";
+
         var anime_in_array = [];
 
         for (i in obj) {
@@ -132,13 +145,34 @@ client.on("message", async message => {
 
             var json_date = new Date(item.year, (item.month - 1), item.day, item.hour, item.minute, item.second, 0);
             var weeks = weeks_needed(json_date);
-            var countDownDate = json_date.getTime() + (one_week * weeks);
+            var countDownDate = new Date(json_date.getTime() + (one_week * weeks));
+
+            const a = new Date(), b = new Date(`${countDownDate.getUTCFullYear()}-${countDownDate.getUTCMonth() + 1}-${countDownDate.getUTCDate()}`),
+                difference = dateDiffInDays(a, b);
 
             countDownDate = dateFormat(new Date(countDownDate.valueOf()), "dddd, dS, HH:MM"); // Saturday, 9th, 16:46
-            message.channel.send(`**${item.name}**: ` + countDownDate);
+
+            switch (difference) {
+                case 1:
+                    one_day = one_day + `**${item.name}**: ` + countDownDate + "\n";
+                    break;
+                case 2:
+                    two_days = two_days + `**${item.name}**: ` + countDownDate + "\n";
+                    break;
+                default:
+                    oth_days = oth_days + `**${item.name}**: ` + countDownDate + "\n";
+            }
+
             Logging(`**${item.name}**: ` + countDownDate);
         });
+
+        message.channel.send(one_day + two_days + oth_days);
     }
+
+    /*if (command === "test") {
+        removeCallMsg(message);
+        console.log(message);
+    }*/
 
 });
 
