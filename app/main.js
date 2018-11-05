53360e537afab871aaf4b801236d32259d40f0de
pKajan <sknitro@gmail.com>
2018-11-05 17:02:05 +0100

    cron activated

--- app/main.js
+++ app/main.js
@@ -11,7 +11,10 @@ var config = require('../config.json');
 
 // Here we load the anime.json file that contains release dates of our anime shows.
 var anime_file = '../anime.json';
+
+// Random vars that i will need later...or never
 var one_week = 7 * 24 * 60 * 60 * 1000;
+var todayArray;
 
 /**************************************************************************/
 /* FUNCTIONS */
@@ -63,20 +66,144 @@ function dateDiffInDays(a, b) {
 
     return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 }
+
+function timeDiffInMinutes(dt2, dt1) {
+    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= 60;
+
+    return Math.abs(Math.round(diff));
+}
+
+function AnimeTimer(message = null, textoutput = false) {
+    var anime = require(anime_file);
+    var obj = anime;
+    var zero_dayHeader = "```fix\nToday:```\n";
+    var zero_day = "";
+    var one_dayHeader = "\n```fix\nOne Day:```\n";
+    var one_day = "";
+    var two_daysHeader = "\n```fix\nTwo Days:```\n";
+    var two_days = "";
+    var oth_days = "\n```fix\nLater:```\n";
+
+    var anime_in_array = [];
+    var TMPtodayArray = [];
+    for (i in obj) {
+        var valueToPush = {};
+        valueToPush.name = i;
+        valueToPush.year = obj[i]["year"];
+        valueToPush.month = obj[i]["month"];
+        valueToPush.day = obj[i]["day"];
+        valueToPush.hour = obj[i]["hour"];
+        valueToPush.minute = obj[i]["minute"];
+        valueToPush.second = obj[i]["second"];
+        anime_in_array.push(valueToPush);
+    }
+
+    console.log("-----------------------------------");
+    anime_in_array.forEach(function (item) {
+
+        var json_date = new Date(item.year, (item.month - 1), item.day, item.hour, item.minute, item.second, 0);
+        var weeks = weeks_needed(json_date);
+        var countDownDateR = new Date(json_date.getTime() + (one_week * weeks));
+
+        const a = new Date(), b = new Date(`${countDownDateR.getUTCFullYear()}-${countDownDateR.getUTCMonth() + 1}-${countDownDateR.getUTCDate()}`),
+            difference = dateDiffInDays(a, b);
+
+        countDownDate = dateFormat(new Date(countDownDateR.valueOf()), "dddd, dS, HH:MM"); // Saturday, 9th, 16:46
+        var onlyTimeForTodays = dateFormat(new Date(countDownDateR.valueOf()), "HH:MM"); // 16:46
+
+        switch (difference) {
+            case 0:
+                zero_day = zero_day + `**${item.name}**: ` + countDownDate + "\n";
+                TMPtodayArray.push([item.name, onlyTimeForTodays]);
+                break;
+            case 1:
+                one_day = one_day + `**${item.name}**: ` + countDownDate + "\n";
+                break;
+            case 2:
+                two_days = two_days + `**${item.name}**: ` + countDownDate + "\n";
+                break;
+            default:
+                oth_days = oth_days + `**${item.name}**: ` + countDownDate + "\n";
+        }
+
+        Logging(`**${item.name}**: ` + countDownDate);
+    });
+
+    todayArray = TMPtodayArray;
+
+    if (zero_day.length > 1) {
+        zero_day = zero_dayHeader + zero_day;
+    }
+    if (one_day.length > 1) {
+        one_day = one_dayHeader + one_day;
+    }
+    if (two_days.length > 1) {
+        two_days = two_daysHeader + two_days;
+    }
+
+    if (textoutput) {
+        message.channel.send(zero_day + one_day + two_days + oth_days);
+    }
+}
+
+function SendtoAllGuilds(text) {
+    try {
+        let toSay = text;
+        client.guilds.map((guild) => {
+            let found = 0
+            guild.channels.map((c) => {
+                if (found === 0) {
+                    if (c.type === "text") {
+                        if (c.permissionsFor(client.user).has("VIEW_CHANNEL") === true) {
+                            if (c.permissionsFor(client.user).has("SEND_MESSAGES") === true) {
+                                c.send(toSay);
+                                found = 1;
+                            }
+                        }
+                    }
+                }
+            });
+        });
+    }
+    catch (err) {
+        console.log("Could not send message to a (few) guild(s)!");
+    }
+}
+
 /**************************************************************************/
 /* STARTUP THINGS */
 client.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully
     Logging(trans("BOT_on_ready", client.users.size, client.channels.size, client.guilds.size));
-    
+
     client.user.setPresence({
-      game: {
-        name: config.activityName,
-        type: config.activityType
-      }
+        game: {
+            name: config.activityName,
+            type: config.activityType
+        }
     }).then(presence => console.log(`Activity set to ${config.activityType + ": " + config.activityName}`))
-      .catch(console.error);
-      
+        .catch(console.error);
+
+    /* CRON */
+    //every 20minutes check
+    const job = new CronJob('*/20 * * * *', function () {
+        AnimeTimer(null, false);
+        var timeNOW = dateFormat(new Date(), "HH:MM"); // 16:46
+        todayArray.forEach(function (item) {
+            console.log(item[1]);
+            // dummy date, I know its today so compare only hours/minutes
+            dt1 = new Date(2018, 10, 1, item[1].split(":")[0], item[1].split(":")[1], 0, 0);
+            dt2 = new Date(2018, 10, 1, timeNOW.split(":")[0], timeNOW.split(":")[1], 0, 0);
+            if (timeDiffInMinutes(dt1, dt2) < 60) {
+                var message = "```fix\nSOON:```\n" + item[0] + " >> " + item[1];
+                SendtoAllGuilds(message);
+            }
+        });
+    });
+
+    job.start();
+    /************************/
 });
 /* Triggered when addeded/removed from server */
 client.on("guildCreate", guild => {
@@ -127,72 +254,8 @@ client.on("message", async message => {
     }
 
     if (command === "anime") {
-        var anime = require(anime_file);
         removeCallMsg(message);
-        var obj = anime;
-        var zero_dayHeader = "```fix\nToday:```\n";
-        var zero_day = "";        
-        var one_dayHeader = "\n```fix\nOne Day:```\n";
-        var one_day = "";
-        var two_daysHeader = "\n```fix\nTwo Days:```\n";
-        var two_days = "";
-        var oth_days = "\n```fix\nLater:```\n";
-
-        var anime_in_array = [];
-
-        for (i in obj) {
-            var valueToPush = {};
-            valueToPush.name = i;
-            valueToPush.year = obj[i]["year"];
-            valueToPush.month = obj[i]["month"];
-            valueToPush.day = obj[i]["day"];
-            valueToPush.hour = obj[i]["hour"];
-            valueToPush.minute = obj[i]["minute"];
-            valueToPush.second = obj[i]["second"];
-            anime_in_array.push(valueToPush);
-        }
-
-        //console.log(anime_in_array);
-        console.log("-----------------------------------");
-        anime_in_array.forEach(function (item) {
-
-            var json_date = new Date(item.year, (item.month - 1), item.day, item.hour, item.minute, item.second, 0);
-            var weeks = weeks_needed(json_date);
-            var countDownDate = new Date(json_date.getTime() + (one_week * weeks));
-
-            const a = new Date(), b = new Date(`${countDownDate.getUTCFullYear()}-${countDownDate.getUTCMonth() + 1}-${countDownDate.getUTCDate()}`),
-                difference = dateDiffInDays(a, b);
-
-            countDownDate = dateFormat(new Date(countDownDate.valueOf()), "dddd, dS, HH:MM"); // Saturday, 9th, 16:46
-
-            switch (difference) {
-				case 0:
-                    zero_day = zero_day + `**${item.name}**: ` + countDownDate + "\n";
-                    break;
-                case 1:
-                    one_day = one_day + `**${item.name}**: ` + countDownDate + "\n";
-                    break;
-                case 2:
-                    two_days = two_days + `**${item.name}**: ` + countDownDate + "\n";
-                    break;
-                default:
-                    oth_days = oth_days + `**${item.name}**: ` + countDownDate + "\n";
-            }
-
-            Logging(`**${item.name}**: ` + countDownDate);
-        });
-        
-        if(zero_day.length > 1){
-        zero_day = zero_dayHeader + zero_day;
-        }
-        if(one_day.length > 1){
-        one_day = one_dayHeader + one_day;
-        }
-        if(two_days.length > 1){
-        two_days = two_daysHeader + two_days;
-        }
-
-        message.channel.send(zero_day + one_day + two_days + oth_days);
+        AnimeTimer(message, true);
     }
 
     /*if (command === "test") {
